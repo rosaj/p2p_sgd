4b0632c83b0dd24f7fd0ec6bfb709d7f347bcc89
Robert Šajina <rosaj@unipu.hr>
2021-12-23 17:41:15 +0100

    Update util.py

--- common/util.py
+++ common/util.py
@@ -2,6 +2,7 @@ import os
 import json
 
 import psutil
+import GPUtil
 
 import random
 import tensorflow as tf
@@ -19,16 +20,21 @@ def set_seed(seed):
 
 def available_device_memory(device_name):
     if 'GPU' in device_name.upper():
-        usage = tf.config.experimental.get_memory_info(device_name)
-        return 10760 - round(usage['current'] / (1024 ** 2), 2)
+        gpu_cur, gpu_total = gpu_memory(device_name, 'MB')
+        return gpu_total - gpu_cur
     elif 'CPU' in device_name.upper():
         return psutil.virtual_memory()[1] / 1024**2
 
 
-def gpu_memory(gpu_name):
+def gpu_memory(gpu_name, units='MB'):
+    power_f = ['B', 'MB', 'GB'].index(units) + 1
+    viz_devs = os.environ["CUDA_VISIBLE_DEVICES"].split(', ')
+    gpu_ind = gpu_name.replace('GPU', '').replace(':', '').strip()
+
     usage = tf.config.experimental.get_memory_info(gpu_name)
-    return {'current (GB)': round(usage['current'] / (1024 ** 3), 2),
-            'peak (GB)': round(usage['peak'] / (1024 * 1024 ** 3), 2)}
+    current = round(usage['current'] / (1024 ** power_f), 2)
+    total = round(GPUtil.getGPUs()[viz_devs[int(gpu_ind)]] * 1024 / (1024 ** power_f))
+    return current, total
 
 
 def memory_info():
@@ -36,7 +42,8 @@ def memory_info():
     mem_dict = {}
     for device in gpu_devices:
         dev_name = device.name.replace('/physical_device:', '')
-        mem_dict[dev_name] = str(gpu_memory(dev_name)['current (GB)']) + ' GB'
+        gpu_cur, gpu_total = gpu_memory(dev_name, 'GB')
+        mem_dict[dev_name] = "{}/{} GB".format(gpu_cur, gpu_total)
     ram_mem = psutil.virtual_memory()
     mem_dict['RAM'] = "{}/{} GB".format(round((ram_mem[0]-ram_mem[1]) / 1024**3, 2), round(ram_mem[0] / 1024**3, 2))
     return mem_dict
