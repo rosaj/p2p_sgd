9e9b198aa13f7c2bfddecb6b18aadfbf57a21fa4
Robert Šajina <rosaj@unipu.hr>
2022-01-20 13:33:44 +0100

    argparse i fl skica

--- test_fl.py
+++ test_fl.py
@@ -1,17 +1,57 @@
+import argparse
 from fl import do_train
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--clients",
+                        required=True,
+                        type=int,
+                        help="Number of clients to be used in simulation")
+    parser.add_argument("--sample_size",
+                        required=True,
+                        type=int,
+                        help="Number of clients to sample in each FL round")
+    parser.add_argument("--epochs",
+                        default=30,
+                        type=int,
+                        help="Number of cumulative epoch to train (default 30)")
+    parser.add_argument("--batch_size",
+                        default=50,
+                        type=int,
+                        help="Batch size (default 50)")
+    parser.add_argument("--seed",
+                        default=None,
+                        type=int,
+                        help="Seed (default None)")
+    parser.add_argument("--model_v",
+                        default=2,
+                        type=int,
+                        help="Model version to be used in simulation (default 2)")
+    parser.add_argument("--slr",
+                        default=0.005,
+                        type=float,
+                        help="Server learning rate (default 0.005)")
+    parser.add_argument("--clr",
+                        default=0.005,
+                        type=float,
+                        help="Client learning rate (default 0.005)")
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    do_train(num_clients=100,
-             num_train_clients=5,
-             batch_size=50,
-             epochs=100,
-             client_pars={"lr": 0.005, "decay": 0},
-             server_pars={"lr": 0.005, "decay": 0},
-             model_v=2,
+    args = parse_args()
+    do_train(num_clients=args.clients,
+             num_train_clients=args.sample_size,
+             batch_size=args.batch_size,
+             epochs=args.epochs,
+             client_pars={"lr": args.slr, "decay": 0},
+             server_pars={"lr": args.clr, "decay": 0},
+             model_v=args.model_v,
              client_weighting='num_examples',
              round_num=None,
-             seed=123,
+             seed=args.seed,
              accuracy_step='epoch'
              )
-
-# /Users/robert/.local/share/virtualenvs/p2p_sgd-oreLDV97/bin/python3.7 test_fl.py > log/fl_50_1000.txt
--- test_p2p.py
+++ test_p2p.py
@@ -1,13 +1,80 @@
+import argparse
+import json
+
 from p2p.train import do_train
-from p2p.agents import P2PAgent, GoSGDAgent, SGPPushSumAgent, D2Agent
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--agent",
+                        default='P2PAgent',
+                        type=str,
+                        required=True,
+                        help="Agent class to be used in simulations")
+    parser.add_argument("--clients",
+                        required=True,
+                        type=int,
+                        help="Number of clients to be used in simulation")
+    parser.add_argument("--batch_size",
+                        default=50,
+                        type=int,
+                        help="Batch size (default 50)")
+    parser.add_argument("--epochs",
+                        default=30,
+                        type=int,
+                        help="Number of cumulative epoch to train (default 30)")
+    parser.add_argument("--seed",
+                        default=None,
+                        type=int,
+                        help="Seed (default None)")
+    parser.add_argument("--model_v",
+                        default=4,
+                        type=int,
+                        help="Model version to be used in simulation (default 4)")
+    parser.add_argument("--lr",
+                        default=0.005,
+                        type=float,
+                        help="Agent learning rate (default 0.005)")
+    parser.add_argument("--agent_pars",
+                        default=None,
+                        type=str,
+                        help="Json-type string with custom agent parameters (default None)")
+    parser.add_argument("--graph_type",
+                        default='sparse',
+                        type=str,
+                        help="Graph type to create as a communication base (default sparse)")
+    parser.add_argument("--neighbors",
+                        default=3,
+                        type=int,
+                        help="Number of neighbors each agent has (default 3)")
+    parser.add_argument("--directed",
+                        action='store_true',
+                        help="Set this flag for directed communication (default false)")
+    parser.add_argument("--vary",
+                        default=-1,
+                        type=int,
+                        help="Time-varying interval of changing communication matrix (default -1)")
+
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    do_train(D2Agent,
-             num_clients=50,
-             batch_size=50,
-             model_pars={"model_v": 4, "lr": 0.005, "default_weights": True},
-             agent_pars=None,
-             graph_pars={'graph_type': 'sparse', 'num_neighbors': 2, 'directed': True, 'time_varying': 1},
-             epochs=40, accuracy_step='epoch')
+    args = parse_args()
+    mod = __import__('p2p.agents').agents
 
-# /Users/robert/.local/share/virtualenvs/p2p_sgd-oreLDV97/bin/python3.7 test_p2p.py > log/p2p_test.txt
+    if not hasattr(mod, args.agent):
+        raise ValueError(f"{args.agent} not found in 'p2p.agents' module")
+
+    do_train(getattr(mod, args.agent),
+             num_clients=args.clients,
+             batch_size=args.batch_size,
+             model_pars={"model_v": args.model_v, "lr": args.lr, "default_weights": True},
+             agent_pars=None if args.agent_pars is None else json.loads(args.agent_pars),
+             graph_pars={'graph_type': args.graph_type,
+                         'num_neighbors': args.neighbors,
+                         'directed': args.directed,
+                         'time_varying': args.vary},
+             epochs=args.epochs,
+             seed=args.seed,
+             accuracy_step='epoch')
