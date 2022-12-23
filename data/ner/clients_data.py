5a4180b5f9033aa732bf7e32beeb5e6dabd6650b
Robert Šajina <rosaj@unipu.hr>
2022-12-23 14:59:27 +0100

    dataset_fraction posibility added

--- data/ner/clients_data.py
+++ data/ner/clients_data.py
@@ -6,7 +6,7 @@ from models.zoo.bert.tokenization import FullTokenizer
 global PROCESSOR
 
 
-def load_clients_data(num_clients, dataset='conll', seq_len=128, vocab_path='data/ner'):
+def load_clients_data(num_clients, dataset='conll', seq_len=128, dataset_fraction=1, vocab_path='data/ner'):
     if dataset == 'conll':
         processor = CoNLLProcessor('data/ner/conll')
     elif dataset == 'few_nerd':
@@ -20,6 +20,14 @@ def load_clients_data(num_clients, dataset='conll', seq_len=128, vocab_path='dat
     train_features = processor.get_train_as_features(seq_len, tokenizer)
     val_features = processor.get_dev_as_features(seq_len, tokenizer)
     test_features = processor.get_test_as_features(seq_len, tokenizer)
+
+    if 0 < dataset_fraction < 1:
+        np.random.shuffle(train_features)
+        np.random.shuffle(val_features)
+        np.random.shuffle(test_features)
+        train_features = train_features[:int(len(train_features) * dataset_fraction)]
+        val_features = val_features[:int(len(val_features) * dataset_fraction)]
+        test_features = test_features[:int(len(test_features) * dataset_fraction)]
 
     data = {
         "train": split_features(train_features, num_clients),
