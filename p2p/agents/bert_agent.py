02c54a6195fc089ae71ad008f937d1a8d1947ed2
Robert Šajina <rosaj@unipu.hr>
2022-12-29 11:15:52 +0100

    multitask bert agent

--- p2p/agents/bert_agent.py
+++ p2p/agents/bert_agent.py
@@ -2,16 +2,19 @@ from p2p.agents.p2p_agent import *
 
 
 class BertAgent(P2PAgent):
-    def __init__(self, **kwargs):
+    def __init__(self, multi_task_optimization=True, **kwargs):
         if 'early_stopping' not in kwargs:
             kwargs['early_stopping'] = False
         super(BertAgent, self).__init__(**kwargs)
+        self.bert_layer = self.model.layers[3]
+        self.multi_task_optimization = multi_task_optimization
 
     def receive_message(self, other_agent):
         super(P2PAgent, self).receive_message(other_agent)
         if self.model.layers[-1].units != other_agent.model.layers[-1].units:
-
-            self.model.layers[3].set_weights(tf.nest.map_structure(lambda a, b: (a + b) / 2.0, self.model.layers[3].get_weights(), other_agent.model.layers[3].get_weights()))
+            self.bert_layer.set_weights(tf.nest.map_structure(lambda a, b: (a + b) / 2.0, self.bert_layer.get_weights(), other_agent.bert_layer.get_weights()))
+            if self.multi_task_optimization:
+                self.bert_layer.trainable = False
         else:
             weights = tf.nest.map_structure(lambda a, b: (a + b) / 2.0, self.get_model_weights(), other_agent.get_model_weights())
             self.set_model_weights(weights)
@@ -20,3 +23,8 @@ class BertAgent(P2PAgent):
         self.train_rounds = 1
 
         return True
+
+    def fit(self, epochs=0):
+        super(BertAgent, self).fit(epochs)
+        if self.multi_task_optimization:
+            self.bert_layer.trainable = True
