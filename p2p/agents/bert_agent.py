f038a1d88a972f85e879992a66b82c763fdb42ab
Robert Šajina <rosaj@unipu.hr>
2022-12-30 12:19:50 +0100

    Update bert_agent.py

--- p2p/agents/bert_agent.py
+++ p2p/agents/bert_agent.py
@@ -2,16 +2,20 @@ from p2p.agents.p2p_agent import *
 
 
 class BertAgent(P2PAgent):
-    def __init__(self, multi_task_optimization=True, **kwargs):
+    def __init__(self, multi_task_optimization=True, multi_task_differentiation='model', **kwargs):
+        assert multi_task_differentiation in ['model', 'data']
         if 'early_stopping' not in kwargs:
             kwargs['early_stopping'] = False
         super(BertAgent, self).__init__(**kwargs)
         self.bert_layer = self.model.layers[3]
         self.multi_task_optimization = multi_task_optimization
+        self.multi_task_differentiation = multi_task_differentiation
 
     def receive_message(self, other_agent):
         super(P2PAgent, self).receive_message(other_agent)
-        if self.model.layers[-1].units != other_agent.model.layers[-1].units:
+
+        if self.multi_task_differentiation == 'model' and self.model.layers[-1].units != other_agent.model.layers[-1].units\
+                or self.multi_task_differentiation == 'data' and self.dataset_name != other_agent.dataset_name:
             self.bert_layer.set_weights(tf.nest.map_structure(lambda a, b: (a + b) / 2.0, self.bert_layer.get_weights(), other_agent.bert_layer.get_weights()))
             if self.multi_task_optimization:
                 self.bert_layer.trainable = False
