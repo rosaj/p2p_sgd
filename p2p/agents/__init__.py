c5ddb4a91c1d9352fbb24808ca1cc9aaeed731b2
Robert Šajina <rosaj@unipu.hr>
2023-07-10 23:23:25 +0200

    Update __init__.py

--- p2p/agents/__init__.py
+++ p2p/agents/__init__.py
@@ -10,3 +10,4 @@ from .pens_agent import PensAgent
 from .dac_agent import DacAgent
 from .gossip_pull_agent import GossipPullAgent
 from .panm_agent import PanmAgent
+from .d_cliques_agent import DCliqueAgent
