4b1bae50d596e7f3ded17f2ae06e4047675512ed
Robert Šajina <rosaj@unipu.hr>
2023-11-07 19:12:34 +0100

    Create gini.py

---
+++ data/metrics/gini.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def gini(x):
+    x = np.array(sorted(x))
+    total = 0
+    for i, xi in enumerate(x[:-1], 1):
+        total += np.sum(np.abs(xi - x[i:]))
+    return total / (len(x)**2 * np.mean(x))
